3ba576f0625d736e1ae8d38c297b826de76a326a
navix <inavix@gmail.com>
2017-10-11 15:29:59 +0300

    feat: add input, textarea, select

--- src/app/kit/kit.module.ts
+++ src/app/kit/kit.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { KitButtonModule } from './kit-button/kit-button.module';
+import { KitInputModule } from './kit-input/kit-input.module';
 import { KitPhotoCarouselModule } from './kit-photo-carousel/kit-photo-carousel.module';
 import { KitQtInputModule } from './kit-qt-input/kit-qt-input.module';
 import { KitRatingModule } from './kit-rating/kit-rating.module';
+import { KitSelectModule } from './kit-select/kit-select.module';
 import { KitSideMenuModule } from './kit-side-menu/kit-side-menu.module';
 import { KitSliderModule } from './kit-slider/kit-slider.module';
+import { KitTextareaModule } from './kit-textarea/kit-textarea.module';
 
 const mods = [
   KitButtonModule,
+  KitInputModule,
   KitPhotoCarouselModule,
   KitQtInputModule,
   KitRatingModule,
+  KitSelectModule,
   KitSideMenuModule,
   KitSliderModule,
+  KitTextareaModule,
 ];
 
 @NgModule({
