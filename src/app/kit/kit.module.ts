227f97690070dbabb9548f3f590aadb64ace1e47
navix <inavix@gmail.com>
2017-10-13 13:08:57 +0300

    feat: add custom-select

--- src/app/kit/kit.module.ts
+++ src/app/kit/kit.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { KitButtonModule } from './kit-button/kit-button.module';
+import { KitCustomSelectModule } from './kit-custom-select/kit-custom-select.module';
 import { KitInputModule } from './kit-input/kit-input.module';
 import { KitPhotoCarouselModule } from './kit-photo-carousel/kit-photo-carousel.module';
 import { KitQtInputModule } from './kit-qt-input/kit-qt-input.module';
@@ -11,6 +12,7 @@ import { KitTextareaModule } from './kit-textarea/kit-textarea.module';
 
 const mods = [
   KitButtonModule,
+  KitCustomSelectModule,
   KitInputModule,
   KitPhotoCarouselModule,
   KitQtInputModule,
--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-preview',
   templateUrl: './preview.component.html',
-  styleUrls: ['./preview.component.scss']
+  styleUrls: ['./preview.component.scss'],
 })
 export class PreviewComponent implements OnInit {
+  options = [
+    {
+      label: 'Option 1',
+      value: 1,
+    },
+    {
+      label: 'Option 2',
+      value: 2,
+    },
+    {
+      label: 'Option 3',
+      value: 3,
+    },
+    {
+      label: 'Option 4',
+      value: 4,
+    },
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
-
 }
