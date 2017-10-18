cc1263792d265e708ec5608f880426232e443e0b
navix <inavix@gmail.com>
2017-10-18 10:32:24 +0300

    feat: reorganize structure (final)

--- src/app/products/cards/cards.ts
+++ src/app/products/cards/cards.ts
@@ -6,7 +6,13 @@ import { ProductCard13Component } from './product-card-13/product-card-13.compon
 import { ProductCard14Component } from './product-card-14/product-card-14.component';
 import { ProductCard15Component } from './product-card-15/product-card-15.component';
 import { ProductCard16Component } from './product-card-16/product-card-16.component';
+import { ProductCard17Component } from './product-card-17/product-card-17.component';
+import { ProductCard18Component } from './product-card-18/product-card-18.component';
+import { ProductCard19Component } from './product-card-19/product-card-19.component';
 import { ProductCard2Component } from './product-card-2/product-card-2.component';
+import { ProductCard20Component } from './product-card-20/product-card-20.component';
+import { ProductCard21Component } from './product-card-21/product-card-21.component';
+import { ProductCard22Component } from './product-card-22/product-card-22.component';
 import { ProductCard3Component } from './product-card-3/product-card-3.component';
 import { ProductCard4Component } from './product-card-4/product-card-4.component';
 import { ProductCard5Component } from './product-card-5/product-card-5.component';
@@ -32,4 +38,10 @@ export const cards = [
   ProductCard14Component,
   ProductCard15Component,
   ProductCard16Component,
+  ProductCard17Component,
+  ProductCard18Component,
+  ProductCard19Component,
+  ProductCard20Component,
+  ProductCard21Component,
+  ProductCard22Component,
 ];
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { UiButtonModule } from './ui-button/ui-button.module';
+import { UiCustomSelectModule } from './ui-custom-select/ui-custom-select.module';
+import { UiInputModule } from './ui-input/ui-input.module';
 import { UiPhotoCarouselModule } from './ui-photo-carousel/ui-photo-carousel.module';
 import { UiQtInputModule } from './ui-qt-input/ui-qt-input.module';
+import { UiRatingModule } from './ui-rating/ui-rating.module';
+import { UiSelectModule } from './ui-select/ui-select.module';
 import { UiSideMenuModule } from './ui-side-menu/ui-side-menu.module';
+import { UiSliderModule } from './ui-slider/ui-slider.module';
+import { UiTextareaModule } from './ui-textarea/ui-textarea.module';
 
 const mods = [
   UiButtonModule,
+  UiCustomSelectModule,
+  UiInputModule,
   UiPhotoCarouselModule,
   UiQtInputModule,
+  UiRatingModule,
+  UiSelectModule,
   UiSideMenuModule,
+  UiSliderModule,
+  UiTextareaModule,
 ];
 
 @NgModule({
