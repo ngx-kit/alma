94882bebd42192d8619016968b3e56c375957f02
icydew <icydew@live.com>
2017-12-04 12:03:37 +0200

    feat: responsive css

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -4,12 +4,16 @@ import { RouterModule } from '@angular/router';
 import { KitOverlayModule } from '@ngx-kit/core';
 import { MainHeaderComponent } from './main-header/main-header.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
+import { UiModule } from '../ui/ui.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
+    FormsModule,
     KitOverlayModule,
+    UiModule,
   ],
   declarations: [
     MainHeaderComponent,
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,11 +2,17 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HomePageComponent } from './home-page/home-page.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { KitOverlayModule } from '@ngx-kit/core';
+import { UiModule } from '../ui/ui.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule,
+    FormsModule,
+    KitOverlayModule,
+    UiModule,
   ],
   declarations: [
     HomePageComponent,
