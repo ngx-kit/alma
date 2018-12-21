1c2e93cd0c683db47edbe0884dcc874085cb3e72
Oleksa Novyk <oleksanovyk@gmail.com>
2018-12-21 15:31:13 +0200

    feat: update to Angular 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule, HAMMER_GESTURE_CONFIG, HammerGestureConfig } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -11,16 +9,6 @@ import { DataService } from './data/data.service';
 import { FooterModule } from './footer/footer.module';
 import { HeaderModule } from './header/header.module';
 import { UiModule } from './ui/ui.module';
-
-export function hammerConfigFactory() {
-  return () => {
-    const config = new HammerGestureConfig();
-    config.overrides = {
-      swipe: {direction: Hammer.DIRECTION_ALL},
-    };
-    return config;
-  };
-}
 
 @NgModule({
   imports: [
@@ -38,10 +26,6 @@ export function hammerConfigFactory() {
   ],
   providers: [
     DataService,
-    {
-      provide: HAMMER_GESTURE_CONFIG,
-      useFactory: hammerConfigFactory(),
-    },
   ],
   bootstrap: [AppComponent],
 })
